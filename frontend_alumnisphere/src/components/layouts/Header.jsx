292427f7260734d8631b7c5a04cc81ba9390960f
Vishal Sharma <vishalsharma485697@gmail.com>
2025-02-28 19:40:19 +0530

    improved

--- frontend_alumnisphere/src/components/layouts/Header.jsx
+++ frontend_alumnisphere/src/components/layouts/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <div className='text-2xl text-white bg-[#243642] h-auto w-full flex justify-center p-2'>
-      <img className='w-[200px]' src="/src\assets\alumnispherelogo.png" alt="" />
+      <img className='w-[200px]' src="/alumnispherelogo.png" alt="" />
       {/* <h1 className='font-semibold font-sans subpixel-antialiased tracking-wide text-justify'>ALUMNI SPHERE</h1> */}
     </div>
   )
