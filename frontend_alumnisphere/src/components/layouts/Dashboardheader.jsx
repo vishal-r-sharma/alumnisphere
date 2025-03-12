f6cb3cc83079f731ac914789f58d8d3627c461af
Vishal Sharma <vishalsharma485697@gmail.com>
2025-03-12 09:55:30 +0530

    improved version

--- frontend_alumnisphere/src/components/layouts/Dashboardheader.jsx
+++ frontend_alumnisphere/src/components/layouts/Dashboardheader.jsx
@@ -102,7 +102,7 @@ function DashboardHeader() {
 
       {/* Mobile Slide-in Menu */}
       <div
-        className={`fixed top-0 right-0 h-full w-4/5 sm:w-1/2 bg-[#1e293b] text-white shadow-lg transform transition-transform duration-300 ease-in-out z-50 ${
+        className={`fixed top-0 right-0 h-full w-4/5 sm:w-1/2 bg-[#243642] text-white shadow-lg transform transition-transform duration-300 ease-in-out z-50 ${
           menuOpen ? "translate-x-0" : "translate-x-full"
         }`}
       >
