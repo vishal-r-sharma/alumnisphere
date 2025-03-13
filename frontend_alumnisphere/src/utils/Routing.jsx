f46cf2b652a37ec89fb9576da07d6d3e2201e475
Vishal Sharma <vishalsharma485697@gmail.com>
2025-03-13 20:57:34 +0530

    Improved

--- frontend_alumnisphere/src/utils/Routing.jsx
+++ frontend_alumnisphere/src/utils/Routing.jsx
@@ -27,7 +27,6 @@ function Routing() {
       <Route path="/dashboard/jobs" element={<Jobs />} />
       <Route path="/dashboard/stories" element={<Stories />} />
       <Route path="/dashboard/alumnidirectory" element={<Alumnidirectory />} />
-      
       {/* 404 Catch-All Route */}
       <Route path="*" element={<Error404 />} />
     </Routes>
